92fc64fee5ac16519802c6bc31d6e7a6edad5989
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2023-07-19 04:10:41 +0600

    adding SampleMeetingNotesApp.swift with the starting point

---
+++ Meeting Notes/SampleMeetingNotesApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct SampleMeetingNotesApp: App {
+    @StateObject private var dataController = DataController()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext,
+                              dataController.container.viewContext)
+        }
+    }
+}
